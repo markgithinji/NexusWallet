b10a8bda5caa1a1c35861cbd51c54b6578f31fce
markgithinji <57723258+markgithinji@users.noreply.github.com>
2026-01-22 19:04:43 +0300

    Add Compose Material and Material Icons Extended dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.compose.material:material:1.10.1")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
     // Retrofit & OkHttp
     implementation(libs.retrofit)
