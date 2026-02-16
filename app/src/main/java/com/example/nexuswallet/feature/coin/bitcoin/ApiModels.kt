65a6ee0be4a103d47e2217ed3ccef354fefeeaa5
markgithinji <57723258+markgithinji@users.noreply.github.com>
2026-02-16 15:04:36 +0300

    Add `BitcoinApi` interface and associated data models for Bitcoin blockchain interaction.

---
+++ app/src/main/java/com/example/nexuswallet/feature/coin/bitcoin/ApiModels.kt
@@ -0,0 +1,71 @@
+package com.example.nexuswallet.feature.coin.bitcoin
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class AddressResponse(
+    val address: String,
+    @SerialName("chain_stats") val chainStats: ChainStats,
+    @SerialName("mempool_stats") val mempoolStats: MempoolStats
+)
+
+@Serializable
+data class ChainStats(
+    @SerialName("funded_txo_sum") val fundedTxoSum: Long,
+    @SerialName("spent_txo_sum") val spentTxoSum: Long,
+    @SerialName("tx_count") val txCount: Int
+)
+
+@Serializable
+data class MempoolStats(
+    @SerialName("funded_txo_sum") val fundedTxoSum: Long,
+    @SerialName("spent_txo_sum") val spentTxoSum: Long,
+    @SerialName("tx_count") val txCount: Int
+)
+
+@Serializable
+data class UtxoResponse(
+    val txid: String,
+    val vout: Int,
+    val value: Long,
+    val status: StatusResponse
+)
+
+@Serializable
+data class StatusResponse(
+    val confirmed: Boolean,
+    @SerialName("block_height") val blockHeight: Int?,
+    @SerialName("block_hash") val blockHash: String?,
+    @SerialName("block_time") val blockTime: Long?
+)
+
+@Serializable
+data class TransactionResponse(
+    val txid: String,
+    val version: Int,
+    val locktime: Int,
+    val vin: List<VinResponse>,
+    val vout: List<VoutResponse>
+)
+
+@Serializable
+data class VinResponse(
+    val txid: String,
+    val vout: Int,
+    @SerialName("scriptsig") val scriptSig: String? = null,
+    @SerialName("scriptsig_asm") val scriptSigAsm: String? = null,
+    val witness: List<String>? = null,
+    @SerialName("is_coinbase") val isCoinbase: Boolean,
+    val sequence: Long
+)
+
+@Serializable
+data class VoutResponse(
+    @SerialName("scriptpubkey") val scriptPubKey: String? = null,
+    @SerialName("scriptpubkey_asm") val scriptPubKeyAsm: String? = null,
+    @SerialName("scriptpubkey_type") val scriptPubKeyType: String? = null,
+    @SerialName("scriptpubkey_address") val scriptPubKeyAddress: String? = null,
+    val value: Long
+)
---
+++ app/src/main/java/com/example/nexuswallet/feature/coin/bitcoin/BitcoinApi.kt
@@ -0,0 +1,26 @@
+package com.example.nexuswallet.feature.coin.bitcoin
+
+import okhttp3.ResponseBody
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface BitcoinApi {
+    @GET("address/{address}")
+    suspend fun getAddressInfo(@Path("address") address: String): AddressResponse
+
+    @GET("address/{address}/utxo")
+    suspend fun getUtxos(@Path("address") address: String): List<UtxoResponse>
+
+    @GET("tx/{txid}")
+    suspend fun getTransaction(@Path("txid") txid: String): TransactionResponse
+
+    @GET("fee-estimates")
+    suspend fun getFeeEstimates(): Map<String, Double>
+
+    @POST("tx")
+    @Headers("Content-Type: text/plain")
+    suspend fun broadcastTransaction(@Body signedHex: String): ResponseBody
+}
